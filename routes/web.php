cc7824cc713b9524d553a6edac7ab74b6d602c33
Eduardo-Matsunaga <eduardoborgesmatsunaga@gmail.com>
2024-04-18 08:20:06 -0300

    Marca: Views index e create, Controller index e create

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MarcaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/marcas', MarcaController::class);
