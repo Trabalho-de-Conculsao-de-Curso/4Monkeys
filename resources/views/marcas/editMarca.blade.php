89745eca5601fa0b27fd0040737f314c87add2e2
Eduardo-Matsunaga <eduardoborgesmatsunaga@gmail.com>
2024-04-18 23:43:49 -0300

    Marca: Views: search e Edit; Controller: show, edit, update e destroy

--- resources/views/marcas/editMarca.blade.php
+++ resources/views/marcas/editMarca.blade.php
@@ -7,21 +7,21 @@
 </head>
 <body class="font-sans antialiased dark:bg-black dark:text-white/50">
 
-    <div>
-        <form action="/marcas" method="POST">
+<div>
+
+        <form action="{{ url("/marcas/$marca->id") }}" method="POST">
+            @method("PUT")
+            <h1 class="text-xl font-bold mb-4">Editar Usuario</h1>
             <div>
-                <label for="nome">
-                    Nome
-                </label>
-                <input type="text" name="nome" id="nome" required>
+                <label for="nome">Nome</label>
+                <input type="text" name="nome" id="nome" value="{{$marca->nome}}" required>
             </div>
             <div>
-                <input type="submit" value="Enviar">
+                <input type="submit" value="Editar">
             </div>
             <input type="hidden" name="_token" value="{{ csrf_token() }}" />
         </form>
-    </div>
+</div>
 
 </body>
 </html>
-
