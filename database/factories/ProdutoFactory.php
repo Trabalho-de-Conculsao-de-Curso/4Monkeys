30b230c26bf02b93858bba74b6f7bf258bf40a28
Eduardo-Matsunaga <eduardoborgesmatsunaga@gmail.com>
2024-04-20 03:08:22 -0300

    feat: Adiciona model, factory e migration para Produto

---
+++ database/factories/ProdutoFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Produto>
+ */
+class ProdutoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nome'=> $this->faker->word(),
+            'marca'=> $this->faker->company(),
+            'especificacoes'=> $this->faker->sentence(),
+            'preco'=> $this->faker->randomFloat(4, 10, 1000),
+            'lojasOnline'=> json_encode(['Loja A', 'Loja B', 'Loja C'])
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Marca;
+use App\Models\Produto;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -22,5 +23,6 @@ class DatabaseSeeder extends Seeder
 //        ]);
 
         Marca::factory()->createOne();
+        Produto::factory()->createOne();
     }
 }
