f7c56094a3de44d13d9c46af8636d2d593136f1f
Eduardo-Matsunaga <eduardoborgesmatsunaga@gmail.com>
2024-04-20 03:08:22 -0300

    feat: Adiciona model, factory e migration para Produto

---
+++ database/migrations/2024_04_20_053615_create_produtos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome',120);
+            $table->string('marca',120);
+            $table->string('especificacoes',120);
+            $table->float('preco');
+            $table->json('lojasOnline')->nullable();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
