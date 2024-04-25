ab6ac4542c187040ba813d4e7851fcbe753c7724
Eduardo-Matsunaga <eduardoborgesmatsunaga@gmail.com>
2024-04-25 17:19:49 -0300

    feat: Ajustes ProdutoController para relacionamento, Ajustes nas views de produto

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -8,9 +8,7 @@ use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
         $produtos= Produto::all();
@@ -21,54 +19,46 @@ class ProdutoController extends Controller
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('produtos.createProduto');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
         $request->validate([
             'nome' => 'required',
-            'marca' => 'required',
+            'marca_nome' => 'required',
+            'marca_qualidade' => 'required',
+            'marca_garantia' => 'required',
             'especificacoes' => 'required',
             'preco' => 'required',
             'lojasOnline' => 'required'
         ]);
 
+        $marca = new Marca();
+        $marca -> nome = $request->input('marca_nome');
+        $marca -> qualidade = $request->input('marca_qualidade');
+        $marca -> garantia = $request->input('marca_garantia');
+        $marca -> save();
+
         $produto = new Produto();
         $produto ->nome = $request ->input('nome');
-        $produto ->marca = $request ->input('marca');
+        $produto ->marca_id = $marca ->id;
         $produto ->especificacoes = $request ->input('especificacoes');
         $produto ->preco = $request ->input('preco');
         $produto ->lojasOnline = $request ->input('lojasOnline');
+        $produto->marca_id = $marca->id;
         $produto ->save();
 
-        $marca = new Marca();
-        $marca->nome = $request->input('marca');
-        $marca->produto_id = $produto->id;
-        $marca->save();
 
-        // Verifique se o produto foi criado com sucesso
-        if ($produto) {
-            return redirect('/produtos')->with('success', 'Produto criado com sucesso.');
-        } else {
-            // Se houver algum erro ao criar o produto, redirecione de volta com uma mensagem de erro
-            return back()->withInput()->with('error', 'Erro ao criar o produto.');
-        }
 
+        return redirect('/produtos');
 
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Request $request)
     {
         $search = $request->input('search');
@@ -82,22 +72,27 @@ class ProdutoController extends Controller
         return view('produtos.searchProduto', compact('results'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+
     public function edit($id)
     {
-        $produto= Produto::find($id);
+        $produto= Produto::with('marca')->find($id);
         return view('produtos.editProduto',compact('produto'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update( Request $request,$id)
     {
         $produto = Produto::find($id);
         $produto->update($request->all());
+
+        $marca = Marca::find($produto->marca_id);
+        $marca->update([
+            'nome' => $request->input('marca_nome'),
+            'qualidade' => $request->input('marca_qualidade'),
+            'garantia' => $request->input('marca_garantia'),
+
+        ]);
+
         return redirect()->route('produtos.index');
     }
 
