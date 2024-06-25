434eb66a2f3b2fad2194de4c0c1f98bc423866c5
Mateus Ferreira <mateus.ieq32@gmail.com>
2024-06-25 13:26:26 -0300

    adiciona coluna tipo em Software

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,16 @@
     <meta charset="UTF-8">
     <title>Seleção de Softwares</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script>
+        function toggleDetails(id) {
+            var details = document.getElementById('details-' + id);
+            if (details.style.display === 'none') {
+                details.style.display = 'block';
+            } else {
+                details.style.display = 'none';
+            }
+        }
+    </script>
 </head>
 <body class="bg-gray-100 text-gray-900">
     <div class="container mx-auto py-8">
@@ -12,13 +22,58 @@
         <form action="{{ route('home.selecionar') }}" method="POST" class="bg-white rounded-lg shadow p-6">
             @csrf
             <h2 class="text-2xl font-semibold mb-4">Selecione os Softwares Desejados:</h2>
-            <div class="mb-6">
-                @foreach($softwares as $software)
-                    <div class="flex items-center mb-2">
-                        <input type="checkbox" id="software{{ $software->id }}" name="softwares[]" value="{{ $software->id }}" class="form-checkbox h-5 w-5 text-blue-600">
-                        <label for="software{{ $software->id }}" class="ml-2 text-lg">{{ $software->nome }}</label>
-                    </div>
-                @endforeach
+            <div class="mb-6 grid grid-cols-1 md:grid-cols-3 gap-6">
+                <div class="">
+                    <h3 class="text-xl font-semibold mb-4 ">Jogos</h3>
+                    @foreach($softwares as $software)
+                        @if($software->tipo == 1)
+                            <div class="flex items-center mb-2">
+                                <div class="">
+                                    <input type="checkbox" id="software{{ $software->id }}" name="softwares[]" value="{{ $software->id }}" class="form-checkbox h-5 w-5 text-blue-600">
+                                    <label for="software{{ $software->id }}" class="ml-2 text-lg">{{ $software->nome }}</label>
+                                    <button type="button" onclick="toggleDetails({{ $software->id }})" class="text-blue-500 hover:underline">Ler Mais</button>
+                                    <div id="details-{{ $software->id }}" class="text-sm text-gray-600 mt-2" style="display: none;">
+                                        {{ $software->descricao }}
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
+                <div>
+                    <h3 class="text-xl font-semibold mb-4">Trabalho</h3>
+                    @foreach($softwares as $software)
+                        @if($software->tipo == 2)
+                            <div class="flex items-center mb-2">
+                                <div class="">
+                                    <input type="checkbox" id="software{{ $software->id }}" name="softwares[]" value="{{ $software->id }}" class="form-checkbox h-5 w-5 text-blue-600">
+                                    <label for="software{{ $software->id }}" class="ml-2 text-lg">{{ $software->nome }}</label>
+                                    <button type="button" onclick="toggleDetails({{ $software->id }})" class="text-blue-500 hover:underline">Ler Mais</button>
+                                    <div id="details-{{ $software->id }}" class="text-sm text-gray-600 mt-2" style="display: none;">
+                                            {{ $software->descricao }}
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
+                <div>
+                    <h3 class="text-xl font-semibold mb-4">Dispositivos</h3>
+                    @foreach($softwares as $software)
+                        @if($software->tipo == 3)
+                        <div class="flex items-center mb-2">
+                            <div>
+                                <input type="checkbox" id="software{{ $software->id }}" name="softwares[]" value="{{ $software->id }}" class="form-checkbox h-5 w-5 text-blue-600">
+                                <label for="software{{ $software->id }}" class="ml-2 text-lg">{{ $software->nome }}</label>
+                                <button type="button" onclick="toggleDetails({{ $software->id }})" class="text-blue-500 hover:underline">Ler Mais</button>
+                                <div id="details-{{ $software->id }}" class="text-sm text-gray-600 mt-2" style="display: none;">
+                                        {{ $software->descricao }}
+                                </div>
+                            </div>
+                        </div>
+                        @endif
+                    @endforeach
+                </div>
             </div>
             <button type="submit" class="bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-300">Selecionar Softwares</button>
         </form>
