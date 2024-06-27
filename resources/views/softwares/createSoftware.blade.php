45384df355bbace3c5d4f7349e8ba36c8cf96a25
Matheus Moura de Matos <149022002+MatheusCodex@users.noreply.github.com>
2024-06-27 10:29:57 -0300

    fix: Crud Software

--- resources/views/softwares/createSoftware.blade.php
+++ resources/views/softwares/createSoftware.blade.php
@@ -10,12 +10,24 @@
 <div>
     <form action="/softwares" method="POST">
         <div>
+            <label for="tipo">Tipo do Software: 1 - Jogo, 2 - Trabalho, 3 - Utilitários</label>
+            <br/>
+            <input type="text" name="tipo" id="tipo" required>
+        </div>
+        <div>
             <label for="nome">Nome</label>
+            <br/>
             <input type="text" name="nome" id="nome" required>
         </div>
         <div>
             <label for="descricao">Descrição</label>
+            <br/>
             <input type="text" name="descricao" id="descricao">
+        </div>
+        <div>
+            <label for="descricao">Requisitos</label>
+            <br/>
+            <input type="text" name="requisitos" id="requisitos">
         </div>
         <div>
             <input type="submit"value="Enviar">
