85569bf06505f0c27ae78bd3f9ac859eef3b312e
Mateus Vinicius Ferreira <mateus.ieq32@gmail.com>
2024-06-27 23:44:14 -0300

    Fix: Correcao migration

--- database/migrations/2024_06_12_005933_create_produto_final_table.php
+++ database/migrations/2024_06_12_005933_create_produto_final_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('produto_finals', function (Blueprint $table) {
+        Schema::create('produto_final', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
             $table->string('categoria');
