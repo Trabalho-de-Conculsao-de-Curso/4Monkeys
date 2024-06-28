99ef96a3e96373a7fa29ba9988fa369e5a926fdc
Mateus Vinicius Ferreira <mateus.ieq32@gmail.com>
2024-06-28 20:40:06 -0300

    Feat: Estilizacao Software Fix: correcao Crud Software

--- database/migrations/2024_06_12_005933_create_produto_final_table.php
+++ database/migrations/2024_06_12_005933_create_produto_final_table.php
@@ -33,12 +33,12 @@ return new class extends Migration
             $table->timestamps();
         });*/
 
-        Schema::create('produto_final_software', function (Blueprint $table) {
+        /*Schema::create('produto_final_software', function (Blueprint $table) {
             $table->id();
             $table->foreignId('produto_final_id')->constrained()->onDelete('cascade');
             $table->foreignId('software_id')->constrained('softwares')->onDelete('cascade');
             $table->timestamps();
-        });
+        });*/
     }
 
     /**
