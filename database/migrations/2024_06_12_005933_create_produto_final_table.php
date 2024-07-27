b87351ddc13a061f52bf63ca8a23cca7db9402f8
Eduardo-Matsunaga <eduardoborgesmatsunaga@gmail.com>
2024-07-27 19:36:39 -0300

    fix: Migratios, formato Preco, Calculo de preços

--- database/migrations/2024_06_12_005933_create_produto_final_table.php
+++ database/migrations/2024_06_12_005933_create_produto_final_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,20 +12,43 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('produto_final', function (Blueprint $table) {
-            $table->id();
-            $table->string('nome');
-            $table->string('categoria');
-            $table->integer('preco_total');
-            $table->string('cpu');
-            $table->string('gpu');
-            $table->string('ram');
-            $table->string('hdd');
-            $table->string('fonte');
-            $table->string('placa_mae');
-            $table->string('cooler');
-            $table->timestamps();
-        });
+        if (config('database.default') === 'sqlite') {
+            // Usando SQL bruto para criar a tabela no SQLite
+            DB::statement('
+                CREATE TABLE produto_final (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nome VARCHAR(255),
+                    categoria VARCHAR(255),
+                    preco_total REAL,
+                    cpu VARCHAR(255),
+                    gpu VARCHAR(255),
+                    ram VARCHAR(255),
+                    hdd VARCHAR(255),
+                    fonte VARCHAR(255),
+                    placa_mae VARCHAR(255),
+                    cooler VARCHAR(255),
+                    created_at TIMESTAMP,
+                    updated_at TIMESTAMP
+                )
+            ');
+        } else {
+            // Usando o método Schema para outros bancos de dados
+            Schema::create('produto_final', function (Blueprint $table) {
+                $table->id();
+                $table->string('nome');
+                $table->string('categoria');
+                $table->float('preco_total');
+                $table->string('cpu');
+                $table->string('gpu');
+                $table->string('ram');
+                $table->string('hdd');
+                $table->string('fonte');
+                $table->string('placa_mae');
+                $table->string('cooler');
+                $table->timestamps();
+            });
+        }
+    }
 
        /* Schema::create('produto_final_produto', function (Blueprint $table) {
             $table->id();
@@ -39,7 +63,7 @@ return new class extends Migration
             $table->foreignId('software_id')->constrained('softwares')->onDelete('cascade');
             $table->timestamps();
         });*/
-    }
+
 
     /**
      * Reverse the migrations.
