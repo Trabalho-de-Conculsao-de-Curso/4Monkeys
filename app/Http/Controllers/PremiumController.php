16c4eed3775567fbd91522b743ab57ca16356048
Eduardo-Matsunaga <eduardoborgesmatsunaga@gmail.com>
2024-09-21 02:12:08 -0300

    feat:crud para premium finalizado

--- app/Http/Controllers/PremiumController.php
+++ app/Http/Controllers/PremiumController.php
@@ -12,16 +12,17 @@ class PremiumController extends Controller
      */
     public function index()
     {
-        $users = User::all();
-        return view('premium.index', compact('users'));
+        $usuarios = User::paginate(10); // Paginar 10 usuários por página
+        return view('premium.index', compact('usuarios'));
     }
+
 
     /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-        //
+        return view('premium.createPremium');
     }
 
     /**
@@ -29,38 +30,75 @@ class PremiumController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nome' => 'required',
+            'email' => 'required',
+            'situacao' => 'required',
+        ]);
+        $usuario = new User();
+        $usuario->nome = $request->input('nome');
+        $usuario->email = $request->input('email');
+        $usuario->situacao = $request->input('situacao');
+        $usuario->password = $request->input('password');
+        $usuario->save();
+
+        return redirect()->route('premium.index');
+
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(User $user)
+    public function show(Request $request)
     {
-        //
+        $search = $request->input('search');
+
+        $results = User::where(function($query) use ($search) {
+            $query->where('id', 'like', "%$search%")
+                ->orWhere('nome', 'like', "%$search%")
+                ->orWhere('situacao', 'like', "%$search%")
+            ->orWhere('email', 'like', "%$search%");
+        })
+            ->paginate(10)->appends(['search' => $search]); // Adicionando o parâmetro de busca à paginação
+
+        return view('premium.searchPremium', compact('results'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(User $user)
+    public function edit($id)
     {
-        //
+        $usuarios = User::all()->find($id);
+        return view('premium.editPremium', compact('usuarios'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, User $user)
+    public function update(Request $request, $id)
     {
-        //
+        $usuarios = User::find($id);
+        $usuarios->nome = $request->input('nome');
+        $usuarios->email = $request->input('email');
+        $usuarios->situacao = $request->input('situacao');
+        $usuarios->password = $request->input('password');
+        $usuarios->update($request->all());
+
+        return redirect()->route('usuario-premium.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(User $user)
+    public function destroy($id)
     {
-        //
+        if (request()->has('_token')){
+            $usuarios = User::findOrFail($id);
+            $usuarios->delete();
+            return redirect()->route('usuario-premium.index');
+        } else {
+            return redirect()->route('usuario-premium.index');
+        }
     }
 }
