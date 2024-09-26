62d223044235dd8dc1814e902e91bade1855e2df
Eduardo-Matsunaga <eduardoborgesmatsunaga@gmail.com>
2024-09-26 19:54:13 -0300

    feat:adminController

--- routes/web.php
+++ routes/web.php
@@ -36,8 +36,7 @@ Route::resource('/produtos', ProdutoController::class);
 Route::resource('/softwares', SoftwareController::class);
 
 Route::resource('/admin', AdminController::class);
-Route::resource('/create-admin', AdminController::class);
-Route::patch('/create-admin', [AdminController::class, 'createAdmin']);
+
 
 /*Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/admin', [AdminController::class, 'index'])->name('auth.admin.AdminDashboard');
